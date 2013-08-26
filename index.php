42851fbe95557ac692c7c04c6f50e54a43791115
ljie-PI <ljie.pi@gmail.com>
2013-08-26 11:19:33 -0400

    up and run

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
-echo $yii."<br/>";
-echo $config."<br/>";
 
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
--- protected/data/dbgen.php
+++ protected/data/dbgen.php
@@ -1,10 +1,10 @@
 <?php
 
-$dbFile=dirname(__FILE__).'/blog.db';
-$sqlFile=dirname(__FILE__).'/schema.sqlite.sql';
+// $dbFile=dirname(__FILE__).'/blog.db';
+$sqlFile=dirname(__FILE__).'/schema.mysql.sql';
 
-@unlink($dbFile);
-$db=new PDO('sqlite:'.$dbFile);
+// @unlink($dbFile);
+$db=new PDO('mysql:host=127.7.161.1;dbname=c9', 'ljie_pi', '', array( PDO::ATTR_PERSISTENT => false));
 $sqls=file_get_contents($sqlFile);
 foreach(explode(';',$sqls) as $sql)
 {
